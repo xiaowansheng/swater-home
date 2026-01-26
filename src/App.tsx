47c9e8b289c29e3f4bcb50c799a82e72056df39e
xiaowansheng <xiaowansheng@foxmail.com>
2026-01-26 23:26:38 +0800

    feat: 初始化应用并配置路由及页面切换动画

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,58 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, useOutlet, useLocation, ScrollRestoration } from 'react-router-dom';
 import Layout from '@components/Layout';
 import Home from '@pages/Home';
 import About from '@pages/About';
 import Websites from '@pages/Websites';
 import { AnimatePresence, motion } from 'framer-motion';
 
-const AnimatedRoutes: React.FC = () => {
+const AnimatedLayout: React.FC = () => {
   const location = useLocation();
+  const outlet = useOutlet();
 
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={location.pathname}
-        initial={{ opacity: 0, scale: 0.98, filter: 'blur(5px)' }}
-        animate={{ opacity: 1, scale: 1, filter: 'blur(0px)' }}
-        exit={{ opacity: 0, scale: 1.02, filter: 'blur(5px)' }}
-        transition={{ duration: 0.3 }}
-      >
-        <Routes location={location}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/websites" element={<Websites />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </motion.div>
-    </AnimatePresence>
+    <Layout>
+      <ScrollRestoration />
+      <AnimatePresence mode="wait" initial={false}>
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0, scale: 0.98, filter: 'blur(5px)' }}
+          animate={{ opacity: 1, scale: 1, filter: 'blur(0px)' }}
+          exit={{ opacity: 0, scale: 1.02, filter: 'blur(5px)' }}
+          transition={{ duration: 0.3 }}
+        >
+          {outlet}
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
   );
 };
 
+const router = createBrowserRouter([
+  {
+    element: <AnimatedLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/websites",
+        element: <Websites />,
+      },
+      {
+        path: "*",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <AnimatedRoutes />
-      </Layout>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
